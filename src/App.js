6eadfa4a9aa38c621e88dfd7f80b5b4115e7853d
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-07-06 12:59:46 +0900

    課題1

--- src/App.js
+++ src/App.js
@@ -6,19 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hallo warld
       
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+         Hello World
+        </h1>
+        
+        
       </header>
     </div>
   );
