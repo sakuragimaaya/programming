cd8c8457289ee84e7c8cd9653f67f4af0097dca9
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-07-06 13:46:18 +0900

    課題３

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <h1>
-         Hello World
-        </h1>
-        
-        
-      </header>
-    </div>
+   <button>Hello world</button>
   );
 }
 
