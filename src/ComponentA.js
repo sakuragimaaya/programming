ed8c89bdd1a35e8ea6467af5b073bb90ec97f89c
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-07-26 22:41:18 +0900

    NO.2

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Table } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const ComponentA = (props) => {
     const backPage = () => {
@@ -15,6 +16,19 @@ const ComponentA = (props) => {
                 <li><Link to="componentb" >ComponentBへ移動</Link></li>
                 <li><Link to="componentc" >Componentcへ移動</Link></li>
             </ul>
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+            </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
         </>
     );
 };
