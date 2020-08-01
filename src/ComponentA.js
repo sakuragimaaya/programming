4c73afd86a4a236c40729df7116b8133ef80d504
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2020-08-01 13:29:53 +0900

    みんなに教えてもらいながら完成

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
+
+const sampleArray = [
+    { id: 1, name: 'aaa', date: 119921125, no: '-' },
+    { id: 2, name: 'bbb', date: 119921126, no: '-' },
+    { id: 3, name: 'ccc', date: 119921127, no: '-' },
+    { id: 4, name: 'ddd', date: 119921128, no: '-' },
+    { id: 5, name: 'eee', date: 119921129, no: '-' }
+]
 
 const ComponentA = () => {
 
@@ -25,6 +34,33 @@ const ComponentA = () => {
                     <Card.Link href='#'>Another Link</Card.Link>
                 </Card.Body>
             </Card>
+
+            <Table striped bordered hover>
+
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>#</th>
+                    </tr>
+
+                </thead>
+
+                {sampleArray.map((data, index) => {
+                    return (
+                        <tbody key={index}>
+                            <tr>
+                                <td>{data.id}</td>
+                                <td>{data.name}</td>
+                                <td>{data.date}</td>
+                                <td>{data.no}</td>
+                            </tr>
+                        </tbody>
+                    );
+
+                })}
+            </Table>
         </>
     );
 };
